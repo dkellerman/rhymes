82989e6e5a68eb8d0dc1cdeeff1032e842ffd492
David Kellerman <david@silvermoon.org>
2020-02-18 23:09:36 -0500

    ..

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ let config = {};
 
 if (process.env.NODE_ENV === 'development') {
   require('dotenv-flow').config();
+};
 
-  config.env = {
-    ELASTIC_ENDPOINT: process.env.ELASTIC_ENDPOINT,
-    ELASTIC_INDEX: process.env.ELASTIC_INDEX,
-    GA_ID: process.env.GA_ID,
-  };
-}
+config.env = {
+  ELASTIC_ENDPOINT: process.env.ELASTIC_ENDPOINT,
+  ELASTIC_INDEX: process.env.ELASTIC_INDEX,
+  GA_ID: process.env.GA_ID,
+};
 
 module.exports = config;
