690aaec334917037f6105cd9430c7b7a67fd48ea
David Kellerman <david@silvermoon.org>
2020-02-19 00:14:14 -0500

    ..

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,9 +15,9 @@ class MyDocument extends Document {
               <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}></script>
               <script dangerouslySetInnerHTML={{__html: `
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){ dataLayer.push(arguments); }
                 gtag('js', new Date());
-                gtag('config', process.env.GA_ID);
+                gtag('config', '${process.env.GA_ID}');
               `}}></script>
             </>
           )}
