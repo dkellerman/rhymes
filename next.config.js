765d84188180a1a60e27e4f614656e716dde2895
David Kellerman <david@silvermoon.org>
2020-02-18 22:47:15 -0500

    prod conf

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-const isDev = process.env.NODE_ENV === 'development';
+require('dotenv-flow').config();
 
 const config = {
   target: 'serverless',
-};
-
-if (isDev) {
-  require('dotenv-flow').config();
-  config.env = {
+  env: {
     ELASTIC_ENDPOINT: process.env.ELASTIC_ENDPOINT,
     ELASTIC_INDEX: process.env.ELASTIC_INDEX,
     GA_ID: process.env.GA_ID,
-  };
-}
+  },
+};
 
 module.exports = config;
