b9368a87ae19fe6dcaebb9a0821e98a35be516f8
David Kellerman <david@silvermoon.org>
2020-03-07 13:13:50 -0500

    sim skeleton

--- api/similarity.py
+++ api/similarity.py
@@ -1,7 +1,25 @@
+import os
+import glob
+import json
 from flask import Flask, Response, request
+
 app = Flask(__name__)
+
+lyrics = open('./data/lyrics/index.txt').read()
+lines = lyrics.split('\n')
+songs = [
+    open(f).read()
+    for f in glob.glob('./data/lyrics/*.txt')
+    if os.path.basename(f) != 'index.txt'
+]
 
 @app.route('/api/similarity')
 def similarity():
     q = request.args.get('q')
-    return Response(q or 'nada', mimetype="text/html")
+    resp = json.dumps({
+        'q': q,
+        'results': [
+            { 'title': 'Hello world' },
+        ]
+    })
+    return Response(resp, mimetype="application/json")
---
+++ src/pages/similarity.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Head from 'next/head';
+import { NavBar } from '../NavBar';
+
+type SimilarityResults = {
+  q: string;
+  results: Array<{
+    title: string;
+  }>;
+};
+
+const SimilarityPage = ({ initialQuery = '' }) => {
+  const textArea = React.useRef<any>(null);
+  const [query, setQuery] = React.useState(initialQuery);
+  const [results, setResults] = React.useState<SimilarityResults|null>(null);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!query) {
+      setResults(null);
+      return;
+    }
+
+    setLoading(true);
+    fetchSimilarity(query).then(results => {
+      setResults(results);
+      setLoading(false);
+    });
+  }, [query]);
+
+  React.useEffect(() => {
+    // searchInput?.current?.focus();
+  }, [results]);
+
+  return (
+    <>
+      <Head>
+        <title>Similarity | Songisms</title>
+      </Head>
+
+      <NavBar />
+
+      <main className="padding-top">
+        <strong>Similarity Analysis</strong>
+
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            setQuery(textArea && textArea?.current?.value || '')
+          }}
+          style={{width:'80vw', display:'flex', flexDirection:'column'}}>
+          <textarea ref={textArea} rows={5} defaultValue={query}
+            style={{width:'100%'}}>
+          </textarea>
+
+          <button type="submit" style={{alignSelf:'flex-end'}}>
+            Submit
+          </button>
+        </form>
+
+        {loading && '...' || (results && (
+          <>
+            <hr />
+            <pre>
+              {JSON.stringify(results)}
+            </pre>
+          </>
+        ))}
+      </main>
+    </>
+  );
+};
+
+SimilarityPage.getInitialProps = ({ req, query }) => {
+  return {
+    initialQuery: query.q || '',
+  };
+};
+
+async function fetchSimilarity(q): Promise<SimilarityResults> {
+  const resp = await fetch(`/api/similarity?q=${encodeURIComponent(q)}`);
+  const { results } = await resp.json();
+  return results;
+}
+
+export default SimilarityPage;
